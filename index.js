f5a8f7a1b538a42c3ff07fb09e353c6d95ace4b8
sm1l3ycorp <35858904+sm1l3ycorp@users.noreply.github.com>
2021-09-17 15:08:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -236,3 +236,6 @@ const arr = [1,2,3,4,5,6,7,8,9,10,11,12]
 const odds = arr.filter(n => n%2)
 
 console.log(odds)
+
+// filter an array based on values in another array
+arr1.filter(e => arr2.includes(e))
